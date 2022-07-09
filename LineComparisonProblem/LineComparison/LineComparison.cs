7b12bd3ad6e90d5c8e68862db01f319348daab07
ravi04ez <ezekie.em@gmail.com>
2022-07-09 15:46:42 +0530

    Add to Calc Line Length

--- LineComparisonProblem/LineComparison/LineComparison.cs
+++ LineComparisonProblem/LineComparison/LineComparison.cs
@@ -8,5 +8,26 @@ namespace LineComparison
 {
     public class LineComparison
     {
+        double LineLength;
+
+        public void calcLineLength()
+        {
+            // Taking input from user of X-Y Coordinates.
+            Console.WriteLine("Enter the the point 1 Coordinate of X1 :  ");
+            int X1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the the point 2 Coordinate of X2 :  ");
+            int X2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the the point 1 Coordinate of Y2:  ");
+            int Y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the the point 1 Coordinate of Y2 :  ");
+            int Y2 = Convert.ToInt32(Console.ReadLine());
+
+            LineLength= Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2);
+            LineLength= Math.Sqrt(LineLength);
+            Console.WriteLine("\nLength of the Line : {0}",LineLength);
+
+
+        }
+
     }
 }
--- LineComparisonProblem/LineComparison/Program.cs
+++ LineComparisonProblem/LineComparison/Program.cs
@@ -9,6 +9,11 @@ namespace LineComparison
             Console.WriteLine("\t\t\t\t################################################################");
             Console.WriteLine("\t\t\t\t\tWELCOME TO LINE COMPARISON COMPUTATATION PROGRAM");
             Console.WriteLine("\t\t\t\t################################################################");
+           
+            // Creating Instance of Class Line Comparison..
+            LineComparison objLength = new LineComparison();
+            objLength.calcLineLength();
+
             Console.ReadKey();
         }
     }
