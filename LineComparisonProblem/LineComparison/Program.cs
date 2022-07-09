2638a6258e2a07b466584b0b3fe7e3f028a22cea
ravi04ez <ezekie.em@gmail.com>
2022-07-09 15:29:23 +0530

    Welcm Msg of Line Comp. Program

---
+++ LineComparisonProblem/LineComparison/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t################################################################");
+            Console.WriteLine("\t\t\t\t\tWELCOME TO LINE COMPARISON COMPUTATATION PROGRAM");
+            Console.WriteLine("\t\t\t\t################################################################");
+            Console.ReadKey();
+        }
+    }
+}
